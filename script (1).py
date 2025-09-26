9ff13a62e7228dd60adde35e4cf14fe9357d047b
NirajPrabhu <109916709+NirajPrabhu@users.noreply.github.com>
2025-09-26 19:24:32 +0200

    Update script (1).py

--- script (1).py
+++ script (1).py
@@ -6,12 +6,7 @@ class AerospacePortfolio {
         this.currentTypeIndex = 0;
         this.isTyping = false;
         this.typingPhrases = [
-            "Aeronautical Engineer",
-            "Flight Control Specialist",
-            "Aerospace Systems Expert",
-            "CFD Analysis Specialist",
-            "Research & Development Engineer",
-            "MATLAB/Simulink Expert"
+            "Aeronautical Engineer"
         ];
         
         this.initializeApp();
